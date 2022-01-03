bf4f9c011ede48beb05f8823b07dd0cfa266d234
Fr3shBrain <aylanc.onur@gmail.com>
2022-01-03 20:32:33 +0300

    circleArea and circleCircumference with module.exports

---
+++ circle/circle.js
@@ -0,0 +1,15 @@
+const PI = Math.PI;
+
+const circleArea = (r) => {
+  return PI * r;
+};
+
+const circleCircumference = (r) => {
+  return 2 * PI * r;
+};
+
+
+module.exports = {
+    circleArea,
+    circleCircumference
+}
