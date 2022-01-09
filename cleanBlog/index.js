a467996fef156c2b4c53d721c2ed1e5990c9eb99
Fr3shBrain <aylanc.onur@gmail.com>
2022-01-09 22:13:29 +0300

    clean blog initialize

---
+++ cleanBlog/index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+const port = 5000;
+
+app.get("/", (req, res) => {
+  const blog = { id: 1, title: "Blog title", description: "Blog description" };
+  res.send(blog);
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
