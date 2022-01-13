b21e6043ae7b9faf8f0c0a5424c2a6149a77eab6
Fr3shBrain <aylanc.onur@gmail.com>
2022-01-13 22:08:15 +0300

    upload image

--- cleanBlog/index.js
+++ cleanBlog/index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const ejs = require("ejs");
+const fileUpload = require("express-fileupload");
+const fs = require("fs");
 
 const Post = require("./models/Post");
 
@@ -14,6 +16,7 @@ mongoose.connect("mongodb://localhost/cleanBlog", {
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true })); // url deki datayı okumamızı sağlıyor.
 app.use(express.json()); // datayı json a çevirmek için
+app.use(fileUpload());
 
 const port = 5000;
 
@@ -42,8 +45,22 @@ app.get("/add_post", (req, res) => {
 });
 
 app.post("/posts", async (req, res) => {
-  await Post.create(req.body);
-  res.redirect("/");
+  const uploadDir = "public/uploads";
+
+  if (!fs.existsSync(uploadDir)) {
+    fs.mkdirSync(uploadDir);
+  }
+
+  let uploadeImage = req.files.image;
+  let uploadPath = __dirname + "/public/uploads/" + uploadeImage.name;
+
+  uploadeImage.mv(uploadPath, async () => {
+    await Post.create({
+      ...req.body,
+      image: "/uploads/" + uploadeImage.name,
+    }); // body bilgisini Photo modeli sayesinde veritabanında dökümana dönüştürüyoruz.
+    res.redirect("/");
+  });
 });
 
 app.listen(port, () => {
